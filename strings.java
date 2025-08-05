017c29d8afff5fb23c21a039adf6373619293250
harshareddy9899 <harshavardhanr809@gmail.com>
2025-08-05 14:21:50 +0530

    Add files via upload

---
+++ strings.java
@@ -0,0 +1,18 @@
+package oops_skill;
+
+public class strings {
+	public static void main(String[] args) {
+		String s1="Harshavardhan Reddy";
+		String s2="Harshavardhan Reddy";
+		System.out.println(s1==s2);
+		
+		s1=s1+"Reddy";
+		System.out.println(s1==s2);
+		
+		String s3="Harshavardhan Reddy";
+		
+		
+		System.out.println(s1==s3);
+		
+}
+}
