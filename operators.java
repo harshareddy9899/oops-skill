ef6bb5711d21997d00921b7b9cb5e0fb7186beea
harshareddy9899 <harshavardhanr809@gmail.com>
2025-08-05 14:19:37 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,13 @@
+package oops_skill;
+
+public class operators {
+	public static void main(String[] args) {
+		  System.out.println(5<2); //false
+		        System.out.println(5<=2); //false
+		   System.out.println(5>2); //true
+		   System.out.println(5>=2); //true
+		    System.out.println(5==2); //false
+		   System.out.println(5!=2); //true
+		  // TODO Auto-generated method stub
+}
+}
---
+++ terinaryoperators.java
@@ -0,0 +1,9 @@
+package oops_skill;
+
+public class terinaryoperators {
+	public static void main(String[] args)
+	{
+		int x=41;
+		System.out.println((x%2==0)?"even number":"odd number");//
+	}
+}
